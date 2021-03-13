2a775504309e75871e6fb41b146b4246c405ba65
Vcthtuf <Vcthtuf@gmail.com>
2021-03-13 09:51:01 +0500

    Made modal window

---
+++ js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', function () {
+
+
+    // modal window 
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function () {
+        overlay.style.display = 'block';
+        this.classList.add('.more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function () {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = 'visible';
+    });
+
+    // ------------------------------------------------
+
+
+});
+
